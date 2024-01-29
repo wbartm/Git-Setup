821e15c3621f249f1a3abd277dfc110b7483d79e
W Meacham <wbmlaw@gmail.com>
2024-01-28 21:42:16 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,40 @@
+/**
+ * Unit tests for utils module
+ */
+
+// dependencies
+var should = require('should'),
+    sinon = require('sinon');
+
+
+// module under test
+var unit = require('');
+
+
+describe("", function () {
+
+    this.timeout(50);	// milliseconds
+    before(function () {
+        ;
+    });
+
+    after(function () {
+        ;
+    });
+
+    beforeEach(function () {
+        ;
+    });
+
+    afterEach(function () {
+        ;
+    });
+
+    describe("", function () {
+        it("should exist", function (done) {
+            should.exist(unit);
+            done();
+        });
+        it("should ");
+    });
+});
